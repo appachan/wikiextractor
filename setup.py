91e1525f2b51b8fc30ecf3f49b2fc527fa92feae
Appakun <appachan.hotel@gmail.com>
2019-04-09 16:11:46 +0900

    :sparkles: add: enable usage as a module.

--- setup.py
+++ setup.py
@@ -6,16 +6,13 @@ def readme():
 
 setup(
     name='wikiextractor',
-
-    description='A script that extracts and cleans text from a Wikipedia'
-                'database dump',
-    author='Giuseppe Attardi',
-    author_email='attardi@di.unipi.it',
-    version='2.69',
-
+    version='2.70',
+    description='A script that extracts and cleans text from a Wikipedia database dump',
+    author='appachan',
+    author_email='appachan.hotel@gmail.com',
     url='https://github.com/attardi/wikiextractor',
-
     license="GPL 3.0",
     keywords=['text', 'nlp'],
-    scripts=['WikiExtractor.py']
+    packages=['wikiextractor'],
+    install_requires=[]
 )
